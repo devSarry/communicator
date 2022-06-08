b070b621e47b5c4d6285075dfbb4746211a742c2
Jonathan Sarry <jsarry@gmail.com>
2022-06-08 08:40:03 +0300

    initial commit

---
+++ apps/dictate-app/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="dictate-app" />
+      <div />
+    </>
+  );
+}
+
+export default App;
